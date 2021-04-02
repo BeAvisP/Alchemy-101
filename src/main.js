9e6b48324821d1689607f74683cceca56531a65b
Bea Avis <bea.avis.p@gmail.com>
2021-04-02 18:07:02 +0200

    Add basic HTML for splashScreen

--- src/main.js
+++ src/main.js
@@ -11,8 +11,27 @@ const buildDom = (htmlString) =>{
 }
 
 //SplashScreen create/remove
-const createSplashScreen = () =>{}
-const removeSplashScreen = () =>{}
+const createSplashScreen = () =>{
+    //HTML SplashScreen
+    splashScreen = buildDom(`
+        <main>
+            <h1>My Alchemy Book</h1>
+            <div>
+                <p>Instructions</p>
+            </div>
+            <button>Start</button>
+        </main>
+    `);
+    document.body.appendChild(splashScreen);
+
+    //Add eventListener to HTML button
+    const startButton = splashScreen.querySelector('button');
+
+    startButton.addEventListener('click', startGame);
+}
+const removeSplashScreen = () =>{
+    splashScreen.remove();
+}
 
 //GameScreen create/remove
 const creteGameScreen = () =>{}
@@ -21,5 +40,20 @@ const removeGameScreen = () =>{}
 //EndGameScreen create/remove
 const createEndGameScreen = () =>{}
 const removeEndGameScreen = () =>{}
+
+//Setting the game state - start or end game
+const startGame = () => {
+    //removeSplashScreen
+    //Si estamos en endGameScreen -- removeEndGameScreen(); si no createGameScreen();
+
+    //Start a new game instance and call start method
+    game = new Game(gameScreen);
+    // game.start();
+}
+
+const endGame = (score, time) => {
+    // removeGameScreen();
+    // createEndGameScreen(score, time);
+}
 
 window.addEventListener("load", createSplashScreen);
