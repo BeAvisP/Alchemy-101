ee96ea755d9f6975a9eb2e03c881f5305577f471
Bea Avis <bea.avis.p@gmail.com>
2021-04-02 16:50:54 +0200

    main.js basic structure and properties

---
+++ src/main.js
@@ -0,0 +1,21 @@
+let game;
+let splashScreen;
+let gameScreen;
+let endGameScreen;
+
+//Create DOM elements from a string
+const buildDom = (htmlString) =>{}
+
+//SplashScreen create/remove
+const createSplashScreen = () =>{}
+const removeSplashScreen = () =>{}
+
+//GameScreen create/remove
+const creteGameScreen = () =>{}
+const removeGameScreen = () =>{}
+
+//EndGameScreen create/remove
+const createEndGameScreen = () =>{}
+const removeEndGameScreen = () =>{}
+
+window.addEventListener("load", createSplashScreen);
