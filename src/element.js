2afd56213ee6e2ee725e77f37fa4ee4a3d527c1f
Bea Avis <bea.avis.p@gmail.com>
2021-04-03 17:48:45 +0200

    Create Element class with drawElement method

--- src/element.js
+++ src/element.js
@@ -1,3 +1,27 @@
-class Element{
+class Element {
+    constructor(elements, name, canvas){
+        let getElement = elements.filter(el => el.name === name)[0];
+        this.name = getElement.name;
+        this.foundElement = getElement.foundElement;
+        this.combinations = getElement.combinations;
+        this.imgSrc = getElement.imgSrc;
+
+        this.canvas = canvas;
+        this.ctx = this.canvas.getContext("2d");
+    }   
     
+    drawElement(index) {
+		const img = document.createElement('img');
+		img.src = this.imgSrc;
+        let initX = 50;
+        let initY = 50;
+
+        img.onload = () => {
+            if (index === 0) {
+                this.ctx.drawImage(img, initX, initY, 100, 100);
+            } else {
+                this.ctx.drawImage(img, initX + (150*index), initY, 100, 100);
+            }
+        };
+	}
 }
