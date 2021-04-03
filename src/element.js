8cf80742cf803fd08337cd0379c7ec7667bf2afe
Bea Avis <bea.avis.p@gmail.com>
2021-04-04 00:22:35 +0200

    Add areCombinable and getCombination to element class

--- src/element.js
+++ src/element.js
@@ -10,50 +10,60 @@ class Element {
         this.ctx = this.canvas.getContext("2d");
         this.imagePosition = [];
         this.imgSize = 100;
-    }   
+    }  
+    
+    initX = 50;
+    initY = 50;
     
     drawElement(index) {
 		const img = document.createElement('img');
 		img.src = this.imgSrc;
-        let initX = 50;
-        let initY = 50;
-        
-        let newX = initX + (150*index);
+       
+        let newX = this.initX + (150*index);
+        //TODO
+        //if newX will collide with canvas edge --> modify Y and X value
 
-        this.imagePosition = [newX, initY]
+        this.imagePosition = [newX, this.initY]
 
         img.onload = () => {
-            this.ctx.drawImage(img, newX, initY, this.imgSize, this.imgSize);
+            this.ctx.drawImage(img, newX, this.initY, this.imgSize, this.imgSize);
         };
 	}
 
     didCollide(mouseClick) {
-        //get all sides of the img
+        //Get all sides of the img
         const elementLeft = this.imagePosition[0];
         const elementRight = this.imagePosition[0] + this.imgSize;
         const elementTop = this.imagePosition[1];
-        const elementBottom = this.imagePosition[1] + this.imgSize;;
+        const elementBottom = this.imagePosition[1] + this.imgSize;
     
-        // //seleccionamos los 4 laterales del enemigo
-        // const enemyLeft = enemy.x;
-        // const enemyRight = enemy.x + enemy.size;
-        // const enemyTop = enemy.y;
-        // const enemyBottom = enemy.y + enemy.size;
-    
-        //comprobamos si el enemigo ha entrado dentro del jugador por cualquiera de los 4 lados
+        //Check if mouseClick has clicked the element image.
         const crossLeft = mouseClick[0] <= elementRight && mouseClick[0] >= elementLeft;
         const crossRight = mouseClick[0] >= elementLeft && mouseClick[0] <= elementRight;
         const crossBottom = mouseClick[1] >= elementTop && mouseClick[1] <= elementBottom;
         const crossTop = mouseClick[1] <= elementBottom && mouseClick[1] >= elementTop;
-    
-        //solo cuando 1 condición de verticalidad y 1 de horizontalidad se cumplen, podemos considerar que nuestros
-        //cuadrados han colisionado
+
         if ((crossLeft || crossRight) && (crossTop || crossBottom)){
             return true;
         } else {
             return false
-        }
-    
-    
-      }
+        }   
+    }
+
+    areCombinable(element) {
+        console.log("Checking!")
+        let canCombine = false;
+        this.combinations.forEach(combination => {            
+            if (combination.element === element.name) {
+                console.log(combination.result);
+                canCombine = true;
+            }
+        });
+        return canCombine;
+    }
+
+    getCombination(element) {
+        return this.combinations.filter(combination => combination.element === element.name)
+        .map(combination => combination.result);
+    }
 }
