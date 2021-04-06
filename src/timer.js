0bbe5da3e81f4ddd58828998e83766e144b91a84
Bea Avis <bea.avis.p@gmail.com>
2021-04-06 13:24:46 +0200

    Change timeLeft value and clean coments

--- src/timer.js
+++ src/timer.js
@@ -1,39 +1,36 @@
 class Timer {
-    constructor() {
-        this.timeLeft = 260; //seconds
-        this.intervalId;
-    }
+  constructor() {
+    this.timeLeft = 60; //seconds
+    this.intervalId;
+  }
 
-    start() {
-        this.intervalId = setInterval(this.update.bind(this), 1000);
-    }
+  start() {
+    this.intervalId = setInterval(this.update.bind(this), 1000);
+  }
 
-    stop() {
-        clearInterval(this.intervalId);
-    }
+  stop() {
+    clearInterval(this.intervalId);
+  }
 
-    update(){
-        this.timeLeft = this.timeLeft - 1;
-        if(this.timeLeft >= 0) {
-            // console.log(this.timeLeft);
-        } else {
-            // console.log("Timeout");
-        }
-    }
+  update() {
+    this.timeLeft = this.timeLeft - 1;
+  }
 
-    getMinutes(){
-        return Math.floor(this.timeLeft / 60);
-    }
+  getMinutes() {
+    return Math.floor(this.timeLeft / 60);
+  }
 
-    getSeconds(){
-        return Math.floor(this.timeLeft - this.getMinutes()*60);
-    }
+  getSeconds() {
+    return Math.floor(this.timeLeft - this.getMinutes() * 60);
+  }
 
-    formatTimer(time) {
-        return time === 0 ? "00" : time > 0 && time < 10 ? "0" + time : "" + time;
-    }
+  formatTimer(time) {
+    return time === 0 ? "00" : time > 0 && time < 10 ? "0" + time : "" + time;
+  }
 
-    getStringTimer(){
-        return `${this.formatTimer(this.getMinutes())}:${this.formatTimer(this.getSeconds())}`;        
-    }
+  getStringTimer() {
+    return `${this.formatTimer(this.getMinutes())}:${this.formatTimer(
+      this.getSeconds()
+    )}`;
+  }
 }
